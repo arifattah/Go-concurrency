92e5b02c35db85f1b76acdc471accab32dc6781a
arifattah <154418372+arifattah@users.noreply.github.com>
2025-11-03 00:46:47 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Go-concurrency
+
+go 1.25.3
---
+++ waitgroup.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			fmt.Println("Hello Golang")
+			wg.Done()
+
+		}()
+	}
+	wg.Wait()
+}
